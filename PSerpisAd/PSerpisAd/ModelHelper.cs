864f4e572bbd7c38843c91873050764d387bb8ef
ivives <ivives75@gmail.com>
2014-02-04 09:29:17 +0100

    añadido GetUpdate y empezado Save

--- PSerpisAd/PSerpisAd/ModelHelper.cs
+++ PSerpisAd/PSerpisAd/ModelHelper.cs
@@ -27,6 +27,25 @@ namespace Serpis.Ad
 		}
 		
 		
+		public static string GetUpdate(Type type){
+		
+			string KeyName = null;
+			List<string> fieldNames = new List<string>();
+			foreach (PropertyInfo propertyInfo in type.GetProperties()){
+			
+				if (propertyInfo.IsDefined(typeof(KeyAttribute), true))
+					KeyName = propertyInfo.Name.ToLower();
+				else if (propertyInfo.IsDefined (typeof(FieldAttribute), true))
+					fieldNames.Add (propertyInfo.Name.ToLower());
+			}
+			
+			string tableName = type.Name.ToLower();
+			
+			return string.Format("update {0} get {1} where {2}=",
+			                     string.Join(", ", tableName), fieldNames, KeyName);//comprobar
+		}
+		
+		
 		public static object Load(Type type, string id){
 			
 			IDbCommand selectDbCommand = App.Instance.DbConnection.CreateCommand();
@@ -34,16 +53,14 @@ namespace Serpis.Ad
 			IDataReader dataReader = selectDbCommand.ExecuteReader();
 			dataReader.Read(); //lee el primero
 			
-			
 			object obj = Activator.CreateInstance(type);
 			foreach (PropertyInfo propertyInfo in type.GetProperties()){
 				if(propertyInfo.IsDefined (typeof(KeyAttribute), true))
-					propertyInfo.SetValue(obj, id, null);
+					propertyInfo.SetValue(obj, id, null);//falta convertir al tipo
 				else if (propertyInfo.IsDefined (typeof(FieldAttribute), true))
 					propertyInfo.SetValue(obj, dataReader[propertyInfo.Name.ToLower()], null);
 				
 			}
-			
 			
 			dataReader.Close();
 //			Categoria categoria = new Categoria();
@@ -54,32 +71,28 @@ namespace Serpis.Ad
 			return obj;
 		}
 		
-		public static object Save(Type type, string id){
+		
+		public static void Save(Type type, string id){
 			
-			IDbCommand selectDbCommand = App.Instance.DbConnection.CreateCommand();
-			selectDbCommand.CommandText = GetSelect(type) + id;
-			IDataReader dataReader = selectDbCommand.ExecuteReader();
-			dataReader.Read(); //lee el primero
+			IDbCommand updateDbCommand = App.Instance.DbConnection.CreateCommand();
+			updateDbCommand.CommandText = GetUpdate(type) + id;
 			
-			
-			object obj = Activator.CreateInstance(type);
-			foreach (PropertyInfo propertyInfo in type.GetProperties()){
-				if(propertyInfo.IsDefined (typeof(KeyAttribute), true))
-					propertyInfo.SetValue(obj, id, null);
-				else if (propertyInfo.IsDefined (typeof(FieldAttribute), true))
-					propertyInfo.SetValue(obj, dataReader[propertyInfo.Name.ToLower()], null);
-				
-			}
+			updateDbCommand.ExecuteNonQuery();
 			
 			
-			dataReader.Close();
-//			Categoria categoria = new Categoria();
-//			categoria.Id = int.Parse(id);
-//			categoria.nombre = dataReader["nombre"].ToString();
-//			dataReader.Close();
-//			return categoria;
-			return obj;
 		}
+		
+		
+//		public static void Save(Categoria categoria){
+//			IDbCommand updateDbCommand = App.Instance.DbConnection.CreateCommand();
+//			updateDbCommand.CommandText = "update categoria set nombre=@nombre where id=" + categoria.Id;
+//			DbCommandUtil.AddParameter(updateDbCommand, "nombre", categoria.Nombre);
+//
+//			updateDbCommand.ExecuteNonQuery();
+//	
+//		}
+		
+		
 		
 	}
 }
